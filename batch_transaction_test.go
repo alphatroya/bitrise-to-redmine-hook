8e343e31cd9cd37b277ad1b32be2c49ad0459fe2
Alexey Korolev <alphatroya@gmail.com>
2020-11-23 14:51:00 +0300

    Perform done requests transactions async (#2)

---
+++ batch_transaction_test.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"errors"
+	"testing"
+)
+
+func TestBatchSuccessTransaction(t *testing.T) {
+	m := MockDoneMarker{}
+	s := &Settings{}
+	il := &IssuesList{
+		[]*Issue{
+			{},
+			{},
+			{},
+			{},
+			{},
+		},
+	}
+	res := batchTransaction(m, il, s, 5)
+	if len(res.Success) != len(il.Issues) {
+		t.Errorf("Error during test expect: %d\nreceived: %d", len(il.Issues), len(res.Success))
+	}
+}
+
+func TestBatchFailTransaction(t *testing.T) {
+	m := MockDoneMarker{true}
+	s := &Settings{}
+	il := &IssuesList{
+		[]*Issue{
+			{},
+			{},
+			{},
+			{},
+			{},
+		},
+	}
+	res := batchTransaction(m, il, s, 5)
+	if len(res.Failures) != len(il.Issues) {
+		t.Errorf("Error during test expect: %d\nreceived: %d", len(il.Issues), len(res.Failures))
+	}
+}
+
+type MockDoneMarker struct {
+	failable bool
+}
+
+func (m MockDoneMarker) markAsDone(issue *Issue, settings *Settings, buildNumber int) error {
+	if m.failable {
+		return errors.New("Fail")
+	}
+	return nil
+}
